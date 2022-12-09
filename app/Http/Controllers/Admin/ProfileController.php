7cd1a3c49fc75a2d9a07dfbfe63d4a7dbcfbc15f
Nitta_Satoshi <s-nitta@cmind-co.jp>
2022-12-09 20:04:07 +0900

    課題が完了しました。ご確認をお願いします。

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -38,4 +38,16 @@ class ProfileController extends Controller
         return redirect('admin/profile/edit');
     }
     
+    public function index(Request $request)
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title !='') {
+            // 検索されたら検索結果を取得する
+            $posts = Profile::where('name',$cond_title)->get();
+        }else{
+            // それ以外はすべてのプロフィールを取得する
+            $posts = Profile::all();
+        }
+        return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+    }
 }
